c7ed400f43cd30e3ff7766cb93edf1eaa54aaa17
tusharkale1963 <tusharkale1963@gmail.com>
2023-05-28 01:34:28 +0530

    add new value

--- src/com/calculetion/addition.java
+++ src/com/calculetion/addition.java
@@ -7,6 +7,10 @@ public class addition {
 		int b=20;
 		int add=a+b;
 		System.out.println("addition of two number = "+add);
+		a=30;
+		b=40;
+		int add1=a+b;
+		System.out.println("addition of two number = "+add1);
 
 	}
 
