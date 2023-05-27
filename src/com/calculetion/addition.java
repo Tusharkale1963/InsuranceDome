faee335719531f6a5bdcb81570bbc06fca0a6e3a
tusharkale1963 <tusharkale1963@gmail.com>
2023-05-28 01:19:44 +0530

    addition method

---
+++ src/com/calculetion/addition.java
@@ -0,0 +1,13 @@
+package com.calculetion;
+
+public class addition {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int add=a+b;
+		System.out.println("addition of two number = "+add);
+
+	}
+
+}
